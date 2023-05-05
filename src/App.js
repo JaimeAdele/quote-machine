e57ad5dd2a853f15026d154e7f527825a30b0c15
JaimeAdele <jaime.zuspann@gmail.com>
2023-05-05 16:38:21 -0700

    fix first load issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Quote from './components/Quote'
 import quotes from './data/quotes';
 import colors from './data/colors';
@@ -7,10 +7,10 @@ import colors from './data/colors';
 function App() {
   const root = document.querySelector(':root');
 
-  const [color, setColor] = useState(colors[Math.floor(Math.random() + colors.length)]);
-  root.style.setProperty('--main-color', color);
+  const [color, setColor] = useState('rgb(22, 160, 133)');
 
   const [quote, setQuote] = useState(quotes[Math.floor(Math.random() * quotes.length)]);
+  root.style.setProperty('--main-color', color);
 
   const changeQuote = () => {
     setQuote(quotes[Math.floor(Math.random() * quotes.length)]);
@@ -21,7 +21,6 @@ function App() {
   return (
     <div className='App'>
       <Quote color={color} quote={quote} changeQuote={changeQuote} />
-      
     </div>
   );
 }
